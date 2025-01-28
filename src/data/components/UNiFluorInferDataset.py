196fdaf6c2a7f9ac0d816682bbf6a52fecf2bc2e
David (Daavood, داود) Bayani <dbayani@mit.edu>
2025-01-28 13:57:32 +0000

    Adding draft code for a dataloader for the NRRD files one wants to train on. Also added code contracts (requires, ensures).

--- src/data/components/UNiFluorInferDataset.py
+++ src/data/components/UNiFluorInferDataset.py
@@ -9,26 +9,99 @@ import os
 import numpy as np
 from tifffile import imread, imsave
 from scipy.ndimage.interpolation import zoom
-sys.path.append('../../..')
+# sys.path.append('../../..')
 from csbdeep.utils import normalize, axes_dict, axes_check_and_normalize, backend_channels_last, move_channel_for_backend
+import typing;
+from src.utils.contracts import requires, ensures;
+import re;
 
-
+import nrrd; 
 
 class UNiFluorInferDataset(data.Dataset):
-    def __init__(self,numberInstances=23):
-        self.confocalVolumeDims=[322,212,65];
-        self.numChannels_obs=1;
-        self.numChannels_label=3;
-        self.numberInstances=numberInstances;
+
+    @staticmethod
+    def _checkFilePathsLoaded(dirPaths : typing.List[str], subFilePaths : typing.Tuple[str,...]) -> None:
+        requires(isinstance(dirPaths,list));
+        requires(all([isinstance(x,str) for x in dirPaths]));
+        requires(isinstance(subFilePaths, tuple));
+        requires(all([isinstance(x, str) for x in subFilePaths]));
+        if(len(dirPaths) == 0):
+            raise Exception("The list of directory paths is empty.");
+        for thisDirPath in dirPaths:
+            if(not os.path.exists(thisDirPath)):
+                raise Exception(f"Path \"{thisDirPath}\" does not exist.");
+            if(not os.path.isdir(thisDirPath)):
+                raise Exception(f"Path \"{thisDirPath}\" exists but is not a directory.");
+            if(not thisDirPath.endswith("/")):
+                pass
+            for thisSubFile in subFilePaths:
+                if(re.match("^[a-zA-Z0-9_\.]+$", thisSubFile) is None):
+                    raise Exception(\
+                        f"Proposed file name \"{thisSubFile}\" does not match name pattern"+\
+                         " allowed, \"^[a-zA-Z0-9_\.]+$\" .");
+                completePathToFile=thisDirPath+thisSubFile;
+                if(not os.path.exists(completePathToFile)):
+                    raise Exception(f"Path \"{completePathToFile}\" does not exist.");
+                if(not os.path.isfile(completePathToFile)):
+                    raise Exception(f"Path \"{completePathToFile}\" exists but is not a file.");
         return;
 
+
+    def __init__(self,pathToSplitSpecification:str,dtype=torch.float64,neuropal_ch_to_grab_indx=1):
+        requires(isinstance(pathToSplitSpecification, str));
+        requires(len(pathToSplitSpecification)> 0);
+        requires(os.path.exists(pathToSplitSpecification));
+        requires(os.path.isfile(pathToSplitSpecification));
+        self.confocalVolumeDims=[211, 97, 64];
+        self.numChannels_obs=1;
+        self.numChannels_label=1;
+        self.neuropal_ch_to_grab_indx=neuropal_ch_to_grab_indx;
+        self.numChInitialNeuropal=3;
+        self.dtype=dtype;
+        self.priorLoaded=dict();
+        self.pathToSplitSpecification=pathToSplitSpecification;
+        self.usesOfSubFilesAndTheirPaths : typing.Tuple[typing.Tuple[str,str],typing.Tuple[str,str]] = \
+                                                 [("obs", "target"), ("all_red.nrrd", "NeuroPAL.nrrd")];
+        fh=open(self.pathToSplitSpecification,"r");
+        dirPaths=[x for x in fh.split("\n") if (len(x) > 0)];
+        self._checkFilePathsLoaded(dirPaths,self.usesOfSubFilesAndTheirPaths[1]);
+        self._numberInstances=len(dirPaths);
+        self._dirPaths=dirPaths;
+        return;
+
+    def __len__(self) -> int:
+        return self._numberInstances;
+
     def __getitem__(self, idx):
-        obs=torch.zeros([self.numChannels_obs] + self.confocalVolumeDims); #,dtype=torch.float16)
-        target=torch.zeros([self.numChannels_label] + self.confocalVolumeDims); #,dtype=torch.float16)
-        filename="";
+        if(idx < 0):
+            raise Exception("This dataloader does not support negative indexing");
+        if(idx>=len(self._dirPaths)):
+            raise IndexError(f"Specified index {idx} is outside the bounds of list of dirPaths list of length {self._numberInstances}.");
         # obs[:,:,:,:] = idx;
         # target[:,:,:,:] = idx+1;
-        return obs, target ; #, filename
+        if(idx in self.priorLoaded):
+            # BELOW LINE ASSUMES THAT THE CALLER WILL NOT MUTATE THE VALUES 
+            # PROVIDED IN readNRRDs["obs"] AND readNRRDs["target"]
+            return self.priorLoaded[idx];
+        dirName=self._dirPaths[idx];
+        readNRRDs=dict();
+        for thisVar, thisFileName in [("obs", "target"), ("all_red.nrrd", "NeuroPAL.nrrd")]:
+            temp = nrrd.read(dirName + thisFileName, index_order="F");
+            readNRRDs[thisVar] = temp[0];
+            if(thisVar=="target"):
+                assert(temp[0].shape == tuple(self.confocalVolumeDims + [3]));
+                readNRRDs[thisVar] = readNRRDs[thisVar][:,:,:,self.neuropal_ch_to_grab_indx];
+            readNRRDs[thisVar]= torch.from_numpy(readNRRDs[thisVar], dtype=self.dtype);
+            assert(readNRRDs[thisVar].shape == tuple(self.confocalVolumeDims));
+            assert(isinstance(readNRRDs[thisVar] , torch.Tensor));
+            assert(readNRRDs[thisVar].dtype == self.dtype );
+            assert(readNRRDs[thisVar].requires_grad == False );
+        
+        # BELOW LINE ASSUMES THAT THE CALLER WILL NOT MUTATE THE VALUES 
+        # PROVIDED IN readNRRDs["obs"] AND readNRRDs["target"]
+        self.priorLoaded[idx]=(readNRRDs["obs"], readNRRDs["target"]);
+        
+        return readNRRDs["obs"], readNRRDs["target"] ; #, filename
     
     def __len__(self):
         return self.numberInstances;
---
+++ src/utils/contracts.py
@@ -0,0 +1,11 @@
+
+import typing;
+
+def requires(boolStatement: bool) -> None:
+    assert(boolStatement);
+    return;
+
+def ensures(boolStatement: bool) -> None:
+    assert(boolStatement);
+    return;
+
